8f832a0f4d618404e910ab5c8dba3bd4f274f42a
Carl Richmond <richmond.carl@outlook.com>
2017-06-26 17:20:50 -0600

    work on routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,19 @@ Rails.application.routes.draw do
     namespace :v1 do
 
       namespace :items do
-        get '/find_all',   to: 'item#index'
         get '/:id/best_day', to: 'item#best_day'
         get '/most_items', to: 'item#most_items'
         get '/most_revenue', to: 'item#most_revenue'
       end
 
       namespace :merchants do
-
+        get '/:id/revenue', to: 'merchant#revenue'
+        get '/:id/customers_with_pending_invoices', to: 'merchant#customers_with_pending_invoices'
+        get '/:id/favorite_customer', to: 'merchant#favorite_customer'
       end
 
       namespace :customers do
-
+        get '/:id/favorite_merchant', to: 'customer#favorite_merchant'
       end
 
       resources :merchants, only: [:index, :show]
