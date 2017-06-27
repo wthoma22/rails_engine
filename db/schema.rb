185183ba1a051a08e5a0dd255cb10d5de6cfa468
Carl Richmond <richmond.carl@outlook.com>
2017-06-27 11:16:51 -0600

    rake command for seeding is working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170627041052) do
+ActiveRecord::Schema.define(version: 20170627171045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,12 +18,6 @@ ActiveRecord::Schema.define(version: 20170627041052) do
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "invoices", force: :cascade do |t|
-    t.text "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -41,6 +35,10 @@ ActiveRecord::Schema.define(version: 20170627041052) do
     t.text "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "customer_id"
+    t.bigint "merchant_id"
+    t.index ["customer_id"], name: "index_invoices_on_customer_id"
+    t.index ["merchant_id"], name: "index_invoices_on_merchant_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -49,6 +47,8 @@ ActiveRecord::Schema.define(version: 20170627041052) do
     t.integer "unit_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "merchant_id"
+    t.index ["merchant_id"], name: "index_items_on_merchant_id"
   end
 
   create_table "merchants", force: :cascade do |t|
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 20170627041052) do
   end
 
   create_table "transactions", force: :cascade do |t|
-    t.integer "credit_card_number"
+    t.bigint "credit_card_number"
     t.integer "credit_card_expiration_date"
     t.string "result"
     t.bigint "invoice_id"
@@ -66,9 +66,11 @@ ActiveRecord::Schema.define(version: 20170627041052) do
     t.datetime "updated_at", null: false
     t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
   end
-  
-  add_foreign_key "transactions", "invoices"
+
   add_foreign_key "invoice_items", "invoices"
   add_foreign_key "invoice_items", "items"
-
+  add_foreign_key "invoices", "customers"
+  add_foreign_key "invoices", "merchants"
+  add_foreign_key "items", "merchants"
+  add_foreign_key "transactions", "invoices"
 end
