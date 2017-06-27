40ed372ae5a51bf8a5c47a87dd38036341a03f4a
William Thomas <wthoma390@gmail.com>
2017-06-26 21:07:28 -0600

    add merchant test and generate model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170627012113) do
+ActiveRecord::Schema.define(version: 20170627030440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,12 @@ ActiveRecord::Schema.define(version: 20170627012113) do
     t.text "name"
     t.text "description"
     t.integer "unit_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "merchants", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
