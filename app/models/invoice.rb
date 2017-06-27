65098d4a9cb6239ca4860c2755ef183423f5cfca
William Thomas <wthoma390@gmail.com>
2017-06-27 13:05:36 -0600

    pass all model tests and add through assosciation for item and invoices

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,7 +2,8 @@ class Invoice < ApplicationRecord
   validates :customer_id, :status, :merchant_id, presence: true
 
   has_many :transactions
-  has_many :invoice_items
   belongs_to :customer
   belongs_to :merchant
+  has_many :invoice_items
+  has_many :items, :through =>  :invoice_items
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Merchant, type: :model do
   describe "relationships" do
     it { should have_many(:items) }
     it { should have_many(:invoices) }
-    it { should have_many(:invoice_items) }
   end
   describe "validations" do
     it { should validate_presence_of(:name) }
