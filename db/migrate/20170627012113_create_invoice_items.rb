f7dda3f37f0b15248a03a2410d52ae6648945dac
Carl Richmond <richmond.carl@outlook.com>
2017-06-26 21:08:39 -0600

    changes to many to many relationship in migration to fit test harness

--- db/migrate/20170627012113_create_invoice_items.rb
+++ db/migrate/20170627012113_create_invoice_items.rb
@@ -1,10 +1,11 @@
-class CreateInvoicesItems < ActiveRecord::Migration[5.1]
+class CreateInvoiceItems < ActiveRecord::Migration[5.1]
   def change
-    create_table :invoices_items do |t|
+    create_table :invoice_items do |t|
       t.integer :quantity
       t.integer :unit_price
       t.references :item, foreign_key: true
       t.references :invoice, foreign_key: true
+
     end
   end
 end
