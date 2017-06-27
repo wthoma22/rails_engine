b601856fae689203e2a7b7a04d534f8b31b66d6c
William Thomas <wthoma390@gmail.com>
2017-06-27 13:59:34 -0600

    add find tests for customer

--- db/schema.rb
+++ db/schema.rb
@@ -22,15 +22,6 @@ ActiveRecord::Schema.define(version: 20170627171045) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "invoice_items", force: :cascade do |t|
-    t.integer "quantity"
-    t.integer "unit_price"
-    t.bigint "item_id"
-    t.bigint "invoice_id"
-    t.index ["invoice_id"], name: "index_invoice_items_on_invoice_id"
-    t.index ["item_id"], name: "index_invoice_items_on_item_id"
-  end
-
   create_table "invoices", force: :cascade do |t|
     t.text "status"
     t.datetime "created_at", null: false
@@ -39,6 +30,15 @@ ActiveRecord::Schema.define(version: 20170627171045) do
     t.bigint "merchant_id"
     t.index ["customer_id"], name: "index_invoices_on_customer_id"
     t.index ["merchant_id"], name: "index_invoices_on_merchant_id"
+  end
+
+  create_table "invoices_items", force: :cascade do |t|
+    t.integer "quantity"
+    t.integer "unit_price"
+    t.bigint "item_id"
+    t.bigint "invoice_id"
+    t.index ["invoice_id"], name: "index_invoices_items_on_invoice_id"
+    t.index ["item_id"], name: "index_invoices_items_on_item_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -67,10 +67,10 @@ ActiveRecord::Schema.define(version: 20170627171045) do
     t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
   end
 
-  add_foreign_key "invoice_items", "invoices"
-  add_foreign_key "invoice_items", "items"
   add_foreign_key "invoices", "customers"
   add_foreign_key "invoices", "merchants"
+  add_foreign_key "invoices_items", "invoices"
+  add_foreign_key "invoices_items", "items"
   add_foreign_key "items", "merchants"
   add_foreign_key "transactions", "invoices"
 end
---
+++ spec/requests/api/v1/finders/customer_find_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+describe "customers API" do
+  context "fing by" do
+    let!(:customer_1) { create(:customer) }
+
+    it "finds customer by id" do
+      id = (customer_1).id
+
+      get '/api/v1/customers/find', params: { id: id }
+      customer = JSON.parse(response.body)
+
+      expect(response.to be_success)
+      expect(customer['id']).to eq(id)
+    end
+
+    it "finds customer by first name" do
+      first_name = cusomer_1.first_name
+
+      get '/api/v1/customers/find', params: { first_name: first_name }
+      customer = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(custome['first_name']).to eq(first_name)
+    end
+
+    it 'finds customer by last name' do
+      last_name = customer_1.last_name
+
+      get '/api/v1/customers/find', params: { last_name: last_name }
+      customer = JSON.parst(response.body)
+
+      expect(response).to be_success
+      expect(customer['last_name']).to eq(last_name)
+    end
+
+    it "finds customer from creation date" do
+      created_at = customer_1.created_at
+
+      get 'api/v1/customers/find', params: { created_at: created_at }
+      customer = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(customer['created_at']).to eq(created_at)
+    end
+
+    it 'finds customer from updated date' do
+      updated_at = customer_1.updated_at
+
+      get '/api/v1/customers/find', params: { updated_at: updated_at }
+      customer = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(customer['updated_at']).to eq(updated_at)
+    end
+  end
+end
