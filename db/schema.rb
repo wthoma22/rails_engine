96d1c4371622e17c8fe5fc0f1345791b92a78f5a
William Thomas <wthoma390@gmail.com>
2017-06-26 22:12:29 -0600

    generate model for transactions with migration and spec

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170627034306) do
+ActiveRecord::Schema.define(version: 20170627041052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,17 @@ ActiveRecord::Schema.define(version: 20170627034306) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "transactions", force: :cascade do |t|
+    t.integer "credit_card_number"
+    t.integer "credit_card_expiration_date"
+    t.string "result"
+    t.bigint "invoice_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
+  end
+
   add_foreign_key "invoices_items", "invoices"
   add_foreign_key "invoices_items", "items"
+  add_foreign_key "transactions", "invoices"
 end
