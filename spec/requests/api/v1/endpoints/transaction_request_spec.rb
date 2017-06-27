1ec40136188e15495d54f479131afbe46e18b79f
William Thomas <wthoma390@gmail.com>
2017-06-26 22:06:22 -0600

    create transaction request spec for /api/v1/transactions endpoint

---
+++ spec/requests/api/v1/endpoints/transaction_request_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe "Transaction API" do
+  context "GET /api/v1/transactions" do
+    it "sends a list of transactions" do
+      create_list(:transaction, 3)
+
+      get '/api/v1/transactions'
+
+      expect(response).to have_http_status(200)
+
+      transactions = JSON.parse(response.body)
+      transaction = transactions.first
+
+      expect(transactions.count).to eq(3)
+      expect(transaction).to have_key("credit_card_number")
+      expect(transaction["credit_card_number"]).to be_a String
+      expect(transaction).to have_key("credit_card_expiration_date")
+      expect(transaction["credit_card_expiration_date"]).to be_a String
+      expect(transaction).to have_key("result")
+      expect(transaction["result"]).to be_a String
+      expect(transaction).to have_key("invoice_id")
+      expect(transaction["invoice_id"]).to be_a String
+    end
+  end
+end
