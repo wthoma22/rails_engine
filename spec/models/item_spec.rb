b6cec28120ef7bd04b5800d4fb811b9df09eab7d
William Thomas <wthoma390@gmail.com>
2017-06-27 11:12:10 -0600

    create/update validation and relationship tests for customer, invoice_items, invoice, item, merchant, and transaction

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+  describe "relationships" do
+    it { should belong_to :merchants}
+    it { should have_many :invoice_items}
+  end
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:unit_price) }
+    it { should validate_presence_of(:merchant_id) }
+  end
+end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Merchant, type: :model do
     it { should have_many(:invoices) }
     it { should have_many(:invoice_items) }
   end
-  describe "validations"
+  describe "validations" do
     it { should validate_presence_of(:name) }
+  end
 end
