82074c91e5f6ae79604a5b0dd2ee1407bc648f16
Carl Richmond <richmond.carl@outlook.com>
2017-06-28 08:52:16 -0600

    invoice relationship endpoints complete with tests

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,8 +2,10 @@ class Invoice < ApplicationRecord
   validates :customer_id, :status, :merchant_id, presence: true
 
   has_many :transactions
+
   belongs_to :customer
   belongs_to :merchant
+  
   has_many :invoice_items
   has_many :items, :through =>  :invoice_items
 end
--- config/routes.rb
+++ config/routes.rb
@@ -44,6 +44,11 @@ Rails.application.routes.draw do
         get '/find', to: 'find#show'
         get '/find_all', to: 'find#index'
         get '/random', to: 'random#show'
+        get '/:id/transactions', to: 'transactions#index'
+        get '/:id/invoice_items', to: 'invoice_items#index'
+        get '/:id/items', to: 'items#index'
+        get '/:id/customer', to: 'customer#show'
+        get '/:id/merchant', to: 'merchant#show'
       end
 
       namespace :invoice_items do
