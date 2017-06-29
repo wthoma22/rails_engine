a9a467b2ebea65a3ef495bf668d372df89d2de56
William Thomas <wthoma390@gmail.com>
2017-06-28 21:52:41 -0600

    pass favorite_customer test and set up all merchant BI tests

---
+++ app/controllers/api/v1/merchants/customers_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::Merchants::CustomersController < ApplicationController
+  def index
+    render json: Merchant.find(params["id"]).customers_with_pending_invoices
+  end
+
+  def show
+    render json: Merchant.find(params["id"]).favorite_customer
+  end
+end
---
+++ spec/requests/api/v1/business_intelligence/merchants/favorite_customer_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "GET /api/v1/merchants/:id/favorite_customer" do
+  context "get to merchants favorite customer" do
+    it "returns the customer who has conducted the most total number of successful transactions" do
+      merchant = create(:merchant)
+      customer_1 = create(:customer)
+      customer_2 = create(:customer)
+      customer_1_invoices = create_list(:invoice, 2, customer_id: customer_1.id, merchant_id: merchant.id)
+      customer_2_invoice = create(:invoice, customer_id: customer_2.id, merchant_id: merchant.id)
+      create(:transaction, invoice_id: customer_2_invoice.id, result: "success")
+      customer_1_invoices.each do |invoice|
+        create(:transaction, invoice_id: invoice.id, result: "success")
+      end
+
+      get "/api/v1/merchants/#{merchant.id}/favorite_customer"
+      customer = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(customer).to be_instance_of(Hash)
+      expect(customer["id"]).to eq(customer_1.id)
+    end
+  end
+end
