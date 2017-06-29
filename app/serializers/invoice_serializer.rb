ed3fde6f2021ef5ee7db4f71fc6561b591abb674
Carl Richmond <richmond.carl@outlook.com>
2017-06-28 20:39:14 -0600

    invoices endpoints passing test harness

--- app/serializers/invoice_serializer.rb
+++ app/serializers/invoice_serializer.rb
@@ -1,8 +1,4 @@
 class InvoiceSerializer < ActiveModel::Serializer
-  attributes :id, :customer_id, :status, :merchant_id, :created_at, :updated_at
+  attributes :customer_id, :id, :merchant_id, :status
 
-  belongs_to :customer
-  belongs_to :merchant
-  has_many :invoice_items
-  has_many :transactions
 end
