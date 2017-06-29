bdbf828d43bfd37f3657a93d0c8e4782ed11b2af
William Thomas <wthoma390@gmail.com>
2017-06-28 20:47:32 -0600

    finish best day routes, model and controller - not passing

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
         get '/most_items', to: 'most_items#index'
         get '/most_revenue', to: 'most_revenue#index'
         get '/most_items', to: 'most_items#index'
+        get ':id/best_day', to: 'best_day#show'
       end
 
       namespace :merchants do
