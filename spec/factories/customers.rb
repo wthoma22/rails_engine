6f5f962d70c330241155031ae7901ead52aadec6
Carl Richmond <richmond.carl@outlook.com>
2017-06-29 09:08:24 -0600

    customer finder rspec tests passing again

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :customer do
     first_name "Bob"
     last_name "Ross"
-    created_at "2014-03-27T14:54:02.000Z"
-    updated_at "2014-03-27T14:54:02.000Z"
+    created_at "2014-03-27T14:54:02.000Z".to_datetime
+    updated_at "2014-03-27T14:54:02.000Z".to_datetime
   end
 end
